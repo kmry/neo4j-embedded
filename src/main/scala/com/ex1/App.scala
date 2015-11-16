d582712578e892ebe072c09a4427112d5b2c185a
Apple <Apple@Apple-no-MacBook-Air.local>
2015-11-17 01:39:07 +0900

    fix some

--- src/main/scala/com/ex1/App.scala
+++ src/main/scala/com/ex1/App.scala
@@ -1,38 +1,39 @@
-package ex
+package ex1
 import org.neo4j.graphdb.factory._
 import org.neo4j.graphdb.{DynamicRelationshipType, GraphDatabaseService, Node}
-//kmr
 import org.neo4j.cypher.ExecutionEngine
 
 object App {
+  val db: GraphDatabaseService = new GraphDatabaseFactory().newEmbeddedDatabase("tmp/neo_cypher_test");
+  val engine: ExecutionEngine = new ExecutionEngine(db)
 
-  val db: GraphDatabaseService = new GraphDatabaseFactory().newEmbeddedDatabase("target/db");
-  var engine: ExecutionEngine = new ExecutionEngine(db)
+  val cyphers= Array(
+    """CREATE (movie:Movie { title:"The Matrix",released:1997 })""",
+    """CREATE (movie:Movie { title:"The Matrix",revised:2003 })""",
+    """
+    MATCH (movie:Movie { title:"The Matrix"})
+    RETURN movie
+    """,
+    """
+    MATCH (movie:Movie {title:"The Matrix"})
+    SET movie.japanese="マトリックス"
+    RETURN movie.title, movie.japanese, movie.released
+    """,
+    """ MATCH (movie:Movie { title:"The Matrix"}) RETURN movie"""
+  )
+
+  def op ={
+    val ns = Stream.from(1).iterator //counter
+    cyphers.foreach{ c =>
+      println (s"コマンド${ns.next} :")
+      println (engine.execute(c).dumpToString())
+    }
+  }
   def main(args: Array[String]): Unit = {
-    implicit val graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("/tmp/neo_cypher1");
     println("db running")
-    op2
+    op
     db.shutdown()
     println("db shutdown")
   }
 
-  def createNode() = {
-    db.createNode()
-  }
-
-  def relate(a: Node, b: Node) {
-    a.createRelationshipTo(b, DynamicRelationshipType.withName("r"))
-  }
-
-///////
-  def op2 ={
-    println(
-      engine.execute("""CREATE (movie:Movie { title:"The Matrix",released:1997 })""").dumpToString()
-      + engine.execute("""CREATE (movie:Movie { title:"The Matrix",released:2993 })""").dumpToString()
-    )
-    println(
-      engine.execute("""MATCH (movie:Movie { title:"The Matrix"})
-RETURN movie""").dumpToString()
-    )
-  }
 }
