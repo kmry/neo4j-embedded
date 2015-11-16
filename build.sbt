3445bf8e2ca590f02a10ba007fab579c59707868
Apple <Apple@Apple-no-MacBook-Air.local>
2015-11-17 01:19:21 +0900

    change scala.app to cyphered ver.

--- build.sbt
+++ build.sbt
@@ -1,10 +1,8 @@
-name := "neo4j-emb-javaapi"
+name := "neo4j-emb-scala-cypher"
 
 version := "1.0"
 
-scalaVersion := "2.11.5"
+scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
-  "org.neo4j" % "neo4j" % "2.1.5")
-
-
+  "org.neo4j" % "neo4j" % "2.3.0")
--- src/main/scala/com/ex1/App.scala
+++ src/main/scala/com/ex1/App.scala
@@ -1,72 +1,37 @@
-package org.ex1
-
+package ex
 import org.neo4j.graphdb.factory._
-import org.neo4j.graphdb.{DynamicRelationshipType, GraphDatabaseService}
+import org.neo4j.graphdb.{DynamicRelationshipType, GraphDatabaseService, Node}
+//kmr
+import org.neo4j.cypher.ExecutionEngine
 
 object App {
 
+  val db: GraphDatabaseService = new GraphDatabaseFactory().newEmbeddedDatabase("target/db");
+  var engine: ExecutionEngine = new ExecutionEngine(db)
   def main(args: Array[String]): Unit = {
-    implicit val graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("/tmp/2neo");
+    implicit val graphDb = new GraphDatabaseFactory().newEmbeddedDatabase("/tmp/3neo");
     println("db running")
-    createNodes
-
-    graphDb.shutdown
+    op2
+    db.shutdown()
     println("db shutdown")
   }
 
-  case class Timings(time: Long, nodes: Int)
-
-  def createNodes(implicit graphDb: GraphDatabaseService): Unit = {
-    var totalNodes = 0
-    val stats = collection.mutable.Map[Int, Timings]()
-    for (t <- 1 to 4) {
-      print(s"t: $t ")
-      for (howNested <- List(1, 3, 5, 7, 10, 20, 50)) {
-        var nodes = 0
-        val start = System.currentTimeMillis()
-
-        for (j <- 1 to 100) {
-          val tx = graphDb.beginTx()
-          try {
-            val firstNode = graphDb.createNode()
-            firstNode.setProperty("node", s"root $j")
-
-            for (i <- 1 to 100 * howNested) {
-              val subNode = graphDb.createNode()
-              subNode.setProperty("node", s"$j-$i")
-              firstNode.createRelationshipTo(subNode, DynamicRelationshipType.withName("state"))
-
-              nodes += 1
-            }
-            tx.success()
-          } finally {
-            tx.close()
-          }
-        }
-
-        val offset = System.currentTimeMillis() - start
-        stats += (howNested -> stats.
-          get(howNested).
-          fold(Timings(offset, nodes))(v =>
-          Timings(v.time + offset, v.nodes + nodes)
-          ))
-        totalNodes += nodes
-
-      }
-    }
-
-    println()
-    println("All nodes: " + totalNodes)
-    stats.keys.toList.sorted.foreach { key =>
-      val times = stats.get(key).get
-      printf("Leafs: % 7d Time[sec]: % 8.3f Nodes: % 10d Nodes/sec: % 4d\n",
-        key * 100,
-        (times.time / 1000.0),
-        times.nodes,
-        (times.nodes / times.time))
-    }
+  def createNode() = {
+    db.createNode()
   }
 
+  def relate(a: Node, b: Node) {
+    a.createRelationshipTo(b, DynamicRelationshipType.withName("r"))
+  }
 
-}
-
+///////
+  def op2 ={
+    println(
+      engine.execute("""CREATE (movie:Movie { title:"The Matrix",released:1997 })""").dumpToString()
+    )
+    engine.execute("""CREATE (movie:Movie { title:"The Matrix",released:2993 })""").dumpToString()
+    println(
+      engine.execute("""MATCH (movie:Movie { title:"The Matrix"})
+RETURN movie""").dumpToString()
+    )
+  }
